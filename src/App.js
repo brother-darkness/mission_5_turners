a7888ad26c28b7b48019ae8bae7ae0e740d2f538
Caleb Winters <calebw@missionreadyhq.com>
2022-09-08 16:32:18 +1200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/navbar/navbar";
+import Footer from "./components/footer/footer";
+
+function App() {
+	return (
+		<div className="App">
+			<Navbar />
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
